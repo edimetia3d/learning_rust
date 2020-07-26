9c536ba3536ece688a975d6f54cc555ffb9df945
edimetia3d <edimetia3d@gmail.com>
2020-07-26 19:17:47 +0800

    ref: print error to stderr

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -3,12 +3,12 @@ use mini_grep;
 
 fn main() {
     let cli_arg = mini_grep::opt::parse_args().unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Parsing arguments failed: {}", err);
         process::exit(1);
     });
 
     let filterd_str = mini_grep::grep_on_cli_arg(&cli_arg).unwrap_or_else(|err| {
-        println!("Failed grep in file:{}\n{}", cli_arg.filename, err);
+        eprintln!("Failed grep in file:{}\n{}", cli_arg.filename, err);
         process::exit(2);
     });
     println!("======Found======\n{}", filterd_str);
