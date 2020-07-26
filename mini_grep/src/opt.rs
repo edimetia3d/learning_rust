daac1f4bd4e78daefd72aa331097dc2f1c209afe
edimetia3d <edimetia3d@gmail.com>
2020-07-26 17:25:50 +0800

    test: add a simple unit test for mod "opt"

--- mini_grep/src/opt.rs
+++ mini_grep/src/opt.rs
@@ -14,13 +14,29 @@ impl CLIArgs {
     }
 }
 
-pub fn parse_args() -> Result<CLIArgs, String> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
+fn do_parse_args(arg_vec: &Vec<String>) -> Result<CLIArgs, String> {
+    if arg_vec.len() < 3 {
         return Result::Err(String::from("Not enough input"));
     }
-    match args.len() {
-        3 => Result::Ok(CLIArgs::new(args[1].clone(), args[2].clone())),
+    match arg_vec.len() {
+        3 => Result::Ok(CLIArgs::new(arg_vec[1].clone(), arg_vec[2].clone())),
         _ => Result::Err(String::from("Unkonwn input"))
+    }
+}
+
+pub fn parse_args() -> Result<CLIArgs, String> {
+    let arg_vec: Vec<String> = env::args().collect();
+    return do_parse_args(&arg_vec);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::opt::*;
+
+    #[test]
+    #[should_panic]
+    fn parse_empty() {
+        let empty_vec: Vec<String> = Vec::new();
+        do_parse_args(&empty_vec).unwrap();
     }
 }
