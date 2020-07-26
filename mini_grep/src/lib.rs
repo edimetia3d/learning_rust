5a302fd86e5fc3a89dde91af1c08bfb3c0670802
edimetia3d <edimetia3d@gmail.com>
2020-07-26 17:42:06 +0800

    test: add empty search and unit test

--- mini_grep/src/lib.rs
+++ mini_grep/src/lib.rs
@@ -9,3 +9,25 @@ pub fn grep_on_cli_arg(arg: &CLIArgs) -> Result<String, Box<dyn Error>> {
     let filterd_str = fs::read_to_string(&arg.filename)?;
     return Result::Ok(filterd_str);
 }
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    return vec![];
+}
+
+#[cfg(test)]
+mod test_search {
+    use super::*;
+
+    #[test]
+    fn one_line() {
+        let query = "son";
+        let content = "?\
+        come,\
+        my son,\
+        it is your time";
+
+        let expected = vec!["my son,"];
+
+        assert_eq!(expected, search(query, content));
+    }
+}
